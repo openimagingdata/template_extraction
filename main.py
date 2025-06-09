bd2a31d9f47246ed2fe41aa316155b998d6475af
Ashu Raman <ashuraman1705@gmail.com>
2025-06-09 16:33:10 -0400

    function to open and read txt files

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import pathlib
 
 from dotenv import load_dotenv
 import pydantic_ai
+from pathlib import Path
 
+def read_in_txt(txtfile):
+    health_file = Path(txtfile) #fill in text file
+    contents =  health_file.read_txt()
+    return contents
 def main():
     load_dotenv()
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
