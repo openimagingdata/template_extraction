38c2e13a0065b3baf62ac3c590d2e69f54880435
Ashu Raman <ashuraman1705@gmail.com>
2025-06-11 23:18:34 -0400

    outputting replies from OPENAI agent and making model choice easier

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import pathlib
 
 from dotenv import load_dotenv
 import pydantic_ai
+from pydantic_ai.models.openai import OpenAIModel
 from pathlib import Path
 
 TEMPLATES_DIR = pathlib.Path(__file__).parent / "templates"
+model = OpenAIModel("gpt-4o-mini")
 
 def main():
     load_dotenv()
@@ -14,9 +16,13 @@ def main():
         raise ValueError("OPENAI_API_KEY is not set in the environment variables.")
 
     agent = pydantic_ai.Agent(
-        "openai:gpt4o-mini",
+        model=model,
         system_prompt="""You are a helpful assistant. Answer the user's questions to the best of your ability.""",
     )
+    response = agent.run_sync("Where did the phrase Hello World come from?")
+    print(response.output)
+    # print(response.all_messages())
+    # print(response.usage())
 
 if __name__ == "__main__":
     main()
